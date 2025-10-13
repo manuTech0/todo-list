d97c44557d5b27277c62c9f324b92e6fe46dca20
manuTech0 <manu024595@gmail.com>
2025-10-13 12:17:18 +0700

    fix: window not defined

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,18 +2,21 @@
 
 import { useAuth } from "@/lib/useAuth"
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL || "https://api.manu-tech.my.id"
 
 export default function Login() {
     const router = useRouter()
     const { isAuth, user } = useAuth()
-    const redirectUrl = window.location.origin + "/sign"
-    if(isAuth) {
-        router.push("/app")
-    } else {
-        router.replace(apiUrl + "/?redirect_url=" + redirectUrl)
-    }
+    useEffect(() => {
+        const redirectUrl = window.location.origin + "/sign"
+        if(isAuth) {
+            router.push("/app")
+        } else {
+            router.replace(apiUrl + "/?redirect_url=" + redirectUrl)
+        }
+    }, [])
 
     return (
         <a href={apiUrl}>Redirect to auth</a>
